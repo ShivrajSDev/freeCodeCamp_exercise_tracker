0c542c19e8dcdb0ba05061aaee1c93c09d8ed9e5
ShivrajSDev <166504815+ShivrajSDev@users.noreply.github.com>
2024-04-22 14:49:16 +0100

    feat: Add MongoDB integration, update New User API logic
    Add MongoDB and Body Parser packages
    Add MongoDB logic and schemas
    Update New User API logic

--- index.js
+++ index.js
@@ -1,17 +1,68 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+require('dotenv').config();
 
-app.use(cors())
-app.use(express.static('public'))
+let mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+let User;
+let Exercise;
+
+let userSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: true
+  }
+});
+
+let exerciseSchema = mongoose.Schema({
+  _id: {
+    type: mongoose.Types.ObjectId,
+    required: true
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  duration: {
+    type: Number,
+    required: true
+  },
+  date: Date
+});
+
+User = mongoose.model('User', userSchema);
+Exercise = mongoose.model('Exercise', exerciseSchema);
+
+app.use(cors());
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.static('public'));
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
 app.post('/api/users', function(req, res) {
-  res.json({post: "user"});
-})
+  let newUser = User({username: req.body.username});
+
+  newUser.save(function(err, data) {
+    if(err)
+    {
+      res.json({error: err});
+    }
+    else {
+      res.json({
+        username: data.username,
+        _id: data._id
+      });
+    }
+  });
+});
 
 app.post('/api/users/:_id/exercises', function(req, res) {
   res.json({post: "exercise"});
@@ -28,4 +79,4 @@ app.get('/api/users/:_id/logs', function(req, res) {
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
-})
+});
