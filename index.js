ea6c859d3986fc2496e2d11c47ab3a500cc9ead3
ShivrajSDev <166504815+ShivrajSDev@users.noreply.github.com>
2024-04-22 13:09:08 +0100

    feat: Add initial API logic

--- index.js
+++ index.js
@@ -9,9 +9,22 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+app.post('/api/users', function(req, res) {
+  res.json({post: "user"});
+})
 
+app.post('/api/users/:_id/exercises', function(req, res) {
+  res.json({post: "exercise"});
+});
 
+app.get('/api/users', function(req, res) {
+  res.json({get: "users"});
+});
 
+app.get('/api/users/:_id/logs', function(req, res) {
+  let { from, to, limit } = req.query;
+  res.json({get: "logs", from: from, to: to, limit: limit});
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
